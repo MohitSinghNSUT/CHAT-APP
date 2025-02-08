80bc0195ec931b42ee4e87c254cd65bc8d9cebef
MohitSinghNSUT <118382421+MohitSinghNSUT@users.noreply.github.com>
2025-02-08 18:13:34 +0530

    Update Join.jsx

--- chat_app/Frontend/src/Join.jsx
+++ chat_app/Frontend/src/Join.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import joinCss from "./joinCss.module.css";
 import { io } from "socket.io-client";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export const Join = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit=()=>{
+    if(room && name)
+    navigate(`/chat?name=${name}&room=${room}`)
+  }
   return (
     <>
       <div className={joinCss.joinOuter}>
@@ -13,9 +18,9 @@ export const Join = () => {
         <h1 className={joinCss.heading}>Join</h1>
             <div><input type="text" placeholder="Name" onChange={(event)=>setName(event.target.value)} className={joinCss.input} id="" /></div>
             <div><input type="text" placeholder="Room" onChange={(event)=>setRoom(event.target.value)} className={joinCss.input} id="" /></div>
-            <NavLink to={name&&room?`/chat?name=${name}&room=${room}`:'/'}>
-                    <button type="submit">button</button>
-            </NavLink>
+            {/* <NavLink to={name&&room?`/chat?name=${name}&room=${room}`:'/'}> */}
+                    <button type="submit" onClick={handleSubmit}>button</button>
+            {/* </NavLink> */}
         </div>
       </div>
     </>
